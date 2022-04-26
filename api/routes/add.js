909b69a18cfb2639ad6ae1986e2f6972e778bb05
ru.kongpop <ru.kongpop@hotmail.com>
2022-04-26 12:39:10 +0700

    fix add.js

--- api/routes/add.js
+++ api/routes/add.js
@@ -4,7 +4,7 @@ const signUpTemplateCopy = require('../modles/signupModles');
 const bcrypt = require('bcrypt');
 
 router.put('/', async (req, res) => {
-    signUpTemplateCopy.findOne({username: req.body.username}).activity.walk
+    signUpTemplateCopy.findOne({username: req.body.username})
     .then(date => {
         res.send(data);
     })
