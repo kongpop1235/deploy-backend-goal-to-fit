2c3662a0596f5552cd3617ce657aa3ce94076ffc
ru.kongpop <ru.kongpop@hotmail.com>
2022-04-27 12:36:19 +0700

    update login.js

--- api/routes/login.js
+++ api/routes/login.js
@@ -7,40 +7,33 @@ const bcrypt = require('bcrypt');
 router.post('/', (req, res) => {
     signUpTemplateCopy.findOne({ username: req.body.username })
         .then(data => {
-            // res.send(data)
-            // const pass = req.body.password;
-            // const passcheck = data.password;
-    bcrypt.compare(req.body.password, data.password, (err, result) => {
-        // if (err) throw err;
-        if (result === true) {
-
-            const dataCheck = [
-                { "activity": data.activity },
-                { "_id": data._id},
-                { "username": data.username},
-                { "phone": data.phone}
-            ];
-
-            const loginFinish = {
-                check: true,
-                data: data
-            }
-            // res.send(check);
-            res.send(dataCheck);
-        } else {
-            res.status(401).send("error 401");
-        }
-    })
+            bcrypt.compare(req.body.password, data.password, (err, result) => {
+                if (result === true) {
+                    const dataCheck = {
+                        check: true,
+                        data: {
+                            activity: data.activity,
+                            _id: data._id,
+                            username: data.username,
+                            phone: data.phone,
+                            gender: data.gender,
+                            birth: data.birth,
+                            height: data.height,
+                            weight: data.weight,
+                            calories_gold: data.calories_gold,
+                            date: data.date,
+                            calories_goal: data.calories_goal,
+                        }
+                    };
+                    res.send(dataCheck);
+                } else {
+                    res.status(401).send("error 401");
+                }
+            })
+        })
+        .catch(error => {
+            res.json(error);
+        })
 })
-    .catch(error => {
-        res.json(error);
-    })
-})
 
-
-// router.put('/edit_profile', async (req, res) => {
-// })
-
-
-// router.get('/signin')
 module.exports = router;
