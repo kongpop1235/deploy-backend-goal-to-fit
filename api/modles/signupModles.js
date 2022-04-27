f285f55f27f33c98c9b28c35b4adbaacf32e40d0
ru.kongpop <ru.kongpop@hotmail.com>
2022-04-27 14:50:03 +0700

    fix

--- api/modles/signupModles.js
+++ api/modles/signupModles.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose');
 
 const signUpTemplate = new mongoose.Schema({
-    _id:{
-        type: String,
-    },
     username: {
         type: String,
         required: true,
