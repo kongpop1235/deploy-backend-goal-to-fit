eed8a9d48c45db51ba25b6b51e6f69f6439cee99
ru.kongpop <ru.kongpop@hotmail.com>
2022-04-27 13:27:58 +0700

    test

--- api/routes/add.js
+++ api/routes/add.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const signUpTemplateCopy = require('../modles/signupModles');
 
-router.post('/', async (req, res) => {
-    signUpTemplateCopy.findOne({username: req.body.username})
-    .save(data => {
-        res.send(data.activity);
+router.post('/:_id', async (req, res) => {
+    signUpTemplateCopy.findOne({username: req.body.username}).then(data => {
+        res.send(req.body);
     })
 })
 
