e1af1df37738f1d576d471306e6428a4157be20d
ru.kongpop <ru.kongpop@hotmail.com>
2022-06-11 08:36:08 +0700

    fix

--- api/routes/signup.js
+++ api/routes/signup.js
@@ -17,7 +17,7 @@ router.post('/', async (req, res) => {
         weight: req.body.weight,
         calories_gold: req.body.calories_gold
     })
-    signedUpUser.createIndex({"username": req.body.username}, {unique: true})
+    signedUpUser.createIndex()
         .then(data => {
             res.json(true)
         })
